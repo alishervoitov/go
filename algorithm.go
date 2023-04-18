9a349cd2b102d61d92fe5cd6d2657062de2ddc3b
Alisher <voitovalisher020@gmail.com>
2023-04-18 23:06:13 +0500

    Peremennie

--- algorithm.go
+++ algorithm.go
@@ -6,11 +6,20 @@
 // 	fmt.Println("Hello, world")
 // }
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var age int = 8
+// 	fmt.Println(age)
+// }
+
 package main
 
 import "fmt"
 
 func main() {
-	var age int = 8
-	fmt.Println(age)
+	var number float64 = 222.222
+	fmt.Println(number)
 }
